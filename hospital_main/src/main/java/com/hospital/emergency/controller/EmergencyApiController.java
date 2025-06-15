87686bfe0e06834b4cffb19288233156a489dcda
YOUHEETAE <dbgmlxo01@g.shingu.ac.kr>
2025-06-15 18:17:06 +0900

    hospital_main

--- hospital_main/src/main/java/com/hospital/emergency/controller/EmergencyApiController.java
+++ hospital_main/src/main/java/com/hospital/emergency/controller/EmergencyApiController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.hospital.dto.web.EmergencyResponse;
 import com.hospital.emergency.service.EmergencyApiService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/emergency")
 public class EmergencyApiController {
 
